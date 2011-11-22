3227d0985142942701851b365d93390233149833
Jim Klucar <jklucar@texeltek.com>
2011-11-21 20:45:03 -0500

    commit from local to work via cloud9

--- test/simple.js
+++ test/simple.js
@@ -7,9 +7,8 @@ var master = thrift.createClient(accumulo.services.master, connection);
 //                cloudtrace.tinfo info, security.AuthInfo
 // getMasterStats(tinfo, credentials, callback)
 var tinfo = new accumulo.ttypes.cloudtrace.TInfo();
-var creds = new accumulo.ttypes.security.AuthInfo('root','secret','9959f958-fe52-4522-ad24-2a9cbfdf7db8');
-master.getMasterStats(tinfo, creds, function(args){ console.log(args) });
+var creds = new accumulo.ttypes.security.AuthInfo('root','secret','cloudbase');
+master.getMasterStats(tinfo, creds, function(args){ console.log(args); });
 
 
 
- 
