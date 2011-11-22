6665aea81780b1fedb83d33d1ade25a62942cbc3
Jim Klucar <klucar@gmail.com>
2011-11-22 15:28:56 +0000

    done with online playing....

--- test/simple.js
+++ test/simple.js
@@ -1,14 +1,16 @@
 var thrift = require('thrift'),
     accumulo = require('accumulo');
 
-var connection = thrift.createConnection("localhost", "9999");
+process.env.PORT = 9999;
+var connection = thrift.createConnection("0.0.0.0", process.env.PORT);
 var master = thrift.createClient(accumulo.services.master, connection);
 
 //                cloudtrace.tinfo info, security.AuthInfo
 // getMasterStats(tinfo, credentials, callback)
 var tinfo = new accumulo.ttypes.cloudtrace.TInfo();
-var creds = new accumulo.ttypes.security.AuthInfo('root','secret','cloudbase');
+var creds = new accumulo.ttypes.security.AuthInfo({'user':'root','password':'secret','instanceId':'cloudbase'});
 master.getMasterStats(tinfo, creds, function(args){ console.log(args); });
 
+console.log('done.');
 
 
